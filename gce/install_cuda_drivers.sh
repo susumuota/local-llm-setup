44660d7a6afba343659f85ff45b1e14dfbff1603
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-06-27 23:05:02 +0900

    fix: install cuda instead of cuda-drivers.

--- gce/install_cuda_drivers.sh
+++ gce/install_cuda_drivers.sh
@@ -9,7 +9,7 @@ echo $distribution
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
-sudo apt-get -y install cuda-drivers
-# sudo apt-get -y install cuda
+# sudo apt-get -y install cuda-drivers
+sudo apt-get -y install cuda
 
 echo "run 'sudo reboot' to enable the driver"
